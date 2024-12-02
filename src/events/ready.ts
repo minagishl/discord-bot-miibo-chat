7bf10851c093f536e6623a4dcf5315ff06263768
minagishl <mvxd568.co@icloud.com>
2024-12-03 01:29:11 +0900

    Set status

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,9 +1,12 @@
-import { Events, type Client } from 'discord.js';
+import { Events, type Client, ActivityType } from 'discord.js';
 
 export default {
   name: Events.ClientReady,
   once: true,
   async execute(client: Client): Promise<void> {
+    client.user?.setActivity('Start a chat with a Mention', {
+      type: ActivityType.Playing,
+    });
     // Log the username of the client when it is ready.
     console.log(`Logged in as ${client.user?.username}!`);
   },
